5ee2d3204441bdf7daf4384b2d507220390ed93c
dragos.ioana <dragos.ioana@mta.ro>
2021-12-21 02:35:03 +0200

    Added SSL certificate support from Azure .pfx

--- src/main/java/com/example/demo/SecurityHandler.java
+++ src/main/java/com/example/demo/SecurityHandler.java
@@ -60,7 +60,7 @@ public class SecurityHandler {
 //                .sign(algorithm);
 
 
-        return ResponseHandler.buildTokenResponse(privKey.toString(), "token",id, HttpStatus.OK);
+        return ResponseHandler.buildTokenResponse(Base64.getEncoder().encodeToString(privKey.getEncoded()), "token",id, HttpStatus.OK);
         //return ResponseHandler.buildTokenResponse("ceva", "token",id, HttpStatus.OK);
 
     }
